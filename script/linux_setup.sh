862ee655f385385e5e9a6aab6e667c4792eb6db0
krig6 <ej.ldsm@gmail.com>
2025-03-31 23:42:29 +0800

    refactor: Simplify LazyGit installation

--- script/linux_setup.sh
+++ script/linux_setup.sh
@@ -16,10 +16,11 @@ rm nvim-linux-x86_64.tar.gz
 mkdir -p ~/.config/
 
 # Install Lazygit
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": *"v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit -D -t /usr/local/bin/
+tar -xzf lazygit.tar.gz lazygit
+sudo mv lazygit /usr/local/bin/
+rm lazygit.tar.gz
 
 # Install Alacritty
 echo "Installing Alacritty..."
