fa069b9d2f040869c3549a2564ed18629e346a87
krig6 <ej.ldsm@gmail.com>
2025-04-09 15:36:15 +0800

    chore: Add keymaps.lua and new color scheme
    - `n` and `N` now recenter the screen after jumping to the next/previous
    search result
    - `<C-d>` and `<C-u>` now recenter after half-page scrolling
    - Added Rose Pine theme in color scheme

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,37 @@
 return {
   --gruvbox-material
+  -- {
+  --   "sainnhe/gruvbox-material",
+  --   enabled = true,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.g.gruvbox_material_transparent_background = 1
+  --     vim.g.gruvbox_material_foreground = "original" -- mix, material, original
+  --     vim.g.gruvbox_material_background = "hard" -- soft, medium, hard
+  --     vim.g.gruvbox_material_ui_contrast = "high" -- The contrast of line numbers, indent lines, etc.
+  --     vim.g.gruvbox_material_float_style = "dim" -- Background of floating windows (dim, bright)
+  --     vim.g.gruvbox_material_diagnostic_line_highlight = 1
+  --     vim.g.gruvbox_material_diagnostic_virtual_text = "highlighted"
+  --
+  --     vim.g.gruvbox_material_better_performance = 1
+  --
+  --     vim.cmd.colorscheme("gruvbox-material")
+  --   end,
+  -- },
   {
-    "sainnhe/gruvbox-material",
-    enabled = true,
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
     priority = 1000,
     config = function()
-      vim.g.gruvbox_material_transparent_background = 1
-      vim.g.gruvbox_material_foreground = "original" -- mix, material, original
-      vim.g.gruvbox_material_background = "hard" -- soft, medium, hard
-      vim.g.gruvbox_material_ui_contrast = "high" -- The contrast of line numbers, indent lines, etc.
-      vim.g.gruvbox_material_float_style = "dim" -- Background of floating windows (dim, bright)
-      vim.g.gruvbox_material_diagnostic_line_highlight = 1
-      vim.g.gruvbox_material_diagnostic_virtual_text = "highlighted"
-
-      vim.g.gruvbox_material_better_performance = 1
-
-      vim.cmd.colorscheme("gruvbox-material")
+      require("rose-pine").setup({
+        disable_background = true,
+        styles = {
+          bold = true,
+          italic = true,
+        },
+      })
+      vim.cmd("colorscheme rose-pine")
     end,
   },
 
