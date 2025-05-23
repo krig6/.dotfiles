715d9b2308cfb75c715f942dbf33b83d8d2deff5
krig6 <ej.ldsm@gmail.com>
2025-05-24 02:38:07 +0800

    chore(nvim): Clean up keymaps and simplify editor settings
    - Removed redundant keymap for '@' in 'keymaps.lua'
    - Simplified indentation and wrapping options in 'set.lua'

--- nvim/lua/ekodes/config/keymaps.lua
+++ nvim/lua/ekodes/config/keymaps.lua
@@ -4,9 +4,7 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
--- Beginning and end of the line
 vim.keymap.set("n", "!", "^")
-vim.keymap.set("n", "@", "$")
 
 -- Select all
 vim.keymap.set("n", "<C-a>", "gg<S-v>G")
--- nvim/lua/ekodes/config/set.lua
+++ nvim/lua/ekodes/config/set.lua
@@ -1,12 +1,6 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
 vim.opt.numberwidth = 5
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.wrap = true
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
