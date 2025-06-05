a0df4ebffafbcf58193a9a3cabb958bec38b9141
krig6 <ej.ldsm@gmail.com>
2025-06-05 22:56:14 +0800

    refactor(pomodoro): Clean up Pomodoro script and improve notifications
    - Removed debug 'echo' statements for cleaner output
    - Improved and clarified notification messages
    - Simplified script by removing commented-out code

--- script/pomodoro.sh
+++ script/pomodoro.sh
@@ -1,38 +1,23 @@
 #!/bin/bash
 
 min=${1:-1}
-loops=${2:-1}
+loops=${2:-2}
 cooldown=${3:-1}
 
 sec=$((min * 60))
 cooldown_sec=$((cooldown * 60))
 cooldown_message_sec=$((cooldown_sec * 1000))
 
-# minutes=${min:?⏰ Error: Duration in minutes is required.}
-# message=${msg:?📝 Error: Notification message is required.}
-
-# local errorMessage="Duration must be greater than 0 minutes."
-
-startMessage="Pomodoro time started! Focus for $min minute(s)..."
-breakMessage="Take a break for a minute."
-doneMessage="Done! Type 'g' in the terminal to start it again."
-
-# if [ $min -eq 0 ]; then
-#   echo $errorMessage
-#   return 1
-# fi
-echo "Loops: $loops"
+startMessage="Pomodoro started! Focus for $min minute(s)..."
+breakMessage="Take a break for $cooldown minute(s)."
+doneMessage="Pomodoro ended!"
 
 for ((i = 1; i <= loops; i++)); do
-  echo "Instance: $i"
   notify-send --urgency=critical -t 5000 -a pomo "$startMessage"
   sleep $sec
-  # notify-send --urgency=critical -t 0 -a pomo $breakMessage
 
   if [ $i -lt $loops ]; then
-    # echo "Cooldown for $cooldown minutes(s)..."
-    echo "Break: $i"
-    notify-send --urgency=critical -t "$cooldown_message_sec" -a pomo "$breakMessage"
+    notify-send --urgency=normal -t "$cooldown_message_sec" -a pomo "$breakMessage"
     sleep $cooldown_sec
   fi
 done
