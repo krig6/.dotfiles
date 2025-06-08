ae4776c023240187e1826d2c3f62e7cc7c2d621d
krig6 <ej.ldsm@gmail.com>
2025-06-08 20:27:12 +0800

    feat(script): Improve pomodoro script messages and argument names
    - Renamed variables for clarity
    - Rewrote notification messages with more engaging, humorous text
    - Improved naming consistency and readability

--- script/pomodoro.sh
+++ script/pomodoro.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-min=${1:-1}
-loops=${2:-2}
-cooldown=${3:-1}
+focus_minutes=${1:-25}
+break_minutes=${2:-5}
+cycles=${3:-8}
 
-sec=$((min * 60))
-cooldown_sec=$((cooldown * 60))
-cooldown_message_sec=$((cooldown_sec * 1000))
+focus_seconds=$((focus_minutes * 60))
+break_seconds=$((break_minutes * 60))
+break_notify_timeout=$((break_seconds * 1000))
 
-startMessage="Pomodoro started! Focus for $min minute(s)..."
-breakMessage="Take a break for $cooldown minute(s)."
-doneMessage="Pomodoro ended!"
+start_message="Time to pretend you're productive for $focus_minutes minutes! 💼🍅"
+break_message="Break time! Go stare at the wall or do something equally important for $break_minutes minutes. 🧘‍♂️☕"
+ended_message="You did it! Now go brag about your Pomodoro skills. 🎉🍕"
 
-for ((i = 1; i <= loops; i++)); do
-  notify-send --urgency=critical -t 5000 -a pomo "$startMessage"
-  sleep $sec
+for ((i = 1; i <= cycles; i++)); do
+  notify-send --urgency=critical -t 5000 -a pomo "$start_message"
+  sleep "$focus_seconds"
 
-  if [ $i -lt $loops ]; then
-    notify-send --urgency=normal -t "$cooldown_message_sec" -a pomo "$breakMessage"
-    sleep $cooldown_sec
+  if [ "$i" -lt "$cycles" ]; then
+    notify-send --urgency=normal -t "$break_notify_timeout" -a pomo "$break_message"
+    sleep "$break_seconds"
   fi
 done
 
-notify-send --urgency=critical -t 0 -a pomo "$doneMessage"
+notify-send --urgency=critical -t 0 -a pomo "$ended_message"
