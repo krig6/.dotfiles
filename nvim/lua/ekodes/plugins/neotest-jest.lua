b5d233053e587f9535e70aaed6a4c849eb96cbd6
krig6 <ej.ldsm@gmail.com>
2025-09-25 15:57:04 +0800

    feat(neotest-jest): Integrate neotest keymaps into neotest-jest config
    - Moved neotest keymaps (<leader>tt, <leader>tf, <leader>to, <leader>tl,
    <leader>ts) into plugin config
    - Added `desc` to each mapping for better discoverability

--- nvim/lua/ekodes/plugins/neotest-jest.lua
+++ nvim/lua/ekodes/plugins/neotest-jest.lua
@@ -18,5 +18,30 @@ return {
         })
       )
     end,
+    config = function(_, opts)
+      local neotest = require("neotest")
+      neotest.setup(opts)
+
+      -- Keymaps
+      vim.keymap.set("n", "<leader>tt", function()
+        neotest.run.run()
+      end, { desc = "Run nearest test" })
+
+      vim.keymap.set("n", "<leader>tf", function()
+        neotest.run.run(vim.fn.expand("%"))
+      end, { desc = "Run tests in file" })
+
+      vim.keymap.set("n", "<leader>to", function()
+        neotest.output.open()
+      end, { desc = "Open test output" })
+
+      vim.keymap.set("n", "<leader>tl", function()
+        neotest.run.run_last()
+      end, { desc = "Run last test" })
+
+      vim.keymap.set("n", "<leader>ts", function()
+        neotest.summary.toggle()
+      end, { desc = "Toggle test summary" })
+    end,
   },
 }
