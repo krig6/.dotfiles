35f97833f2a29f1366082cf214a0608812281eb4
krig6 <ej.ldsm@gmail.com>
2025-09-25 16:29:28 +0800

    chore(nvim): Update editor settings
    - Disable writebackup and backup
    - Enable line wrapping
    - Treat `-` as part of a word
    - Enable true color support (`termguicolors = true`)

--- nvim/lua/ekodes/config/set.lua
+++ nvim/lua/ekodes/config/set.lua
@@ -3,6 +3,8 @@ vim.opt.relativenumber = true
 vim.opt.numberwidth = 5
 vim.opt.swapfile = false
 vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 vim.opt.statuscolumn = ""
@@ -11,3 +13,6 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
+vim.opt.wrap = true
+vim.opt.iskeyword:append("-")
+vim.opt.termguicolors = true
