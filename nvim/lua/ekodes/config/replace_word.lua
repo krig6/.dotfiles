7e8653920f959d52c83d82436467492eb8d6d19c
krig6 <ej.ldsm@gmail.com>
2025-09-25 16:15:03 +0800

    refactor(nvim-keymaps): Add word replacement mappings
    - Renamed `replace_current_word.lua` to `replace_word.lua`
    - Added `<leader>cw` to replace text within a visual selection

--- nvim/lua/ekodes/config/replace_word.lua
+++ nvim/lua/ekodes/config/replace_word.lua
@@ -1,16 +1,19 @@
+-- Replace word under cursor
 vim.keymap.set("n", "<leader>rw", function()
   local word = vim.fn.expand("<cword>")
   local replacement = vim.fn.input("Replace '" .. word .. "' with: ")
   if replacement == "" then
     return
   end
-
-  -- Save current cursor position
   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-
-  -- Do the substitution
   vim.cmd(string.format("%%s/\\<%s\\>/%s/g", vim.fn.escape(word, "\\/"), replacement))
-
-  -- Restore cursor position
   vim.api.nvim_win_set_cursor(0, { row, col })
 end, { noremap = true, silent = true })
+
+-- Replace within visual selection
+vim.api.nvim_set_keymap("x", "<leader>cw", [[:<C-u>lua VisualReplaceWord()<CR>]], { noremap = true, silent = true })
+function VisualReplaceWord()
+  local old = vim.fn.input("Old word: ")
+  local new = vim.fn.input("New word: ")
+  vim.cmd("'<,'>s/" .. old .. "/" .. new .. "/g")
+end
--- nvim/lua/ekodes/init.lua
+++ nvim/lua/ekodes/init.lua
@@ -1,3 +1,3 @@
 require("ekodes.config.keymaps")
 require("ekodes.config.set")
-require("ekodes.config.replace_current_word")
+require("ekodes.config.replace_word")
