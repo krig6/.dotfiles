9802f8f50fd6224fd13c2224fe6732035d0eb075
krig6 <ej.ldsm@gmail.com>
2025-09-25 15:31:40 +0800

    refactor(markdown): Update markdown-preview plugin config
    - Changed build step to use `cd app && yarn install`
    - Added configto set `mkdp_filetypes` for markdown
    - Simplified loading by removing explicit cmd option

--- nvim/lua/ekodes/plugins/markdown-preview.lua
+++ nvim/lua/ekodes/plugins/markdown-preview.lua
@@ -1,19 +1,10 @@
 return {
-  -- {
-  --   "iamcco/markdown-preview.nvim",
-  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  --   build = "cd app && npm install", -- Use npm to install dependencies
-  --   init = function()
-  --     vim.g.mkdp_filetypes = { "markdown" } -- Enable the plugin for markdown files
-  --   end,
-  --   ft = { "markdown" }, -- Load plugin only for markdown files
-  -- },
   {
     "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
+    build = "cd app && yarn install",
+    config = function()
+      vim.g.mkdp_filetypes = { "markdown" }
     end,
   },
 }
