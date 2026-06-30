5d4d5c25bf213752b0e2fd40d4feb817e4794913
krig6 <ej.ldsm@gmail.com>
2026-06-30 21:31:58 +0800

    feat(nvim): improve markdown preview configuration
    - Add lazy-loaded MarkdownPreview commands
    - Use an npm-based build function for plugin installation
    - Add a `<leader>mp` keymap to toggle markdown preview
    - Keep the plugin restricted to Markdown filetypes

--- nvim/lua/ekodes/plugins/markdown-preview.lua
+++ nvim/lua/ekodes/plugins/markdown-preview.lua
@@ -2,9 +2,15 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     ft = { "markdown" },
-    build = "cd app && yarn install",
+    cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
+    build = function()
+      vim.fn.system({ "npm", "install" }, { cwd = vim.fn.stdpath("data") .. "/lazy/markdown-preview.nvim/app" })
+    end,
     config = function()
       vim.g.mkdp_filetypes = { "markdown" }
     end,
+    keys = {
+      { "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", mode = "n", desc = "Toggle markdown preview" },
+    },
   },
 }
